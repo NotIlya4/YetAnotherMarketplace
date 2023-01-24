e87d049aa91f2b632bb986329dc2a97929a95c51
NotIlya <secretman44444@gmail.com>
2023-01-24 22:02:29 +0300

    Add ISpecification

---
+++ Core/Specifications/ISpecification.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+
+namespace Core.Specifications;
+
+public interface ISpecification<T>
+{
+    public Expression<Func<T, bool>>? Criteria { get; }
+
+    public List<Expression<Func<T, object>>> Includes { get; }
+}
