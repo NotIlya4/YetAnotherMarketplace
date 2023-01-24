22b75780821df8d79783dae6a92d34f026bea639
NotIlya <secretman44444@gmail.com>
2023-01-24 04:15:40 +0300

    Add IGenericRepository

---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities;
+
+namespace Core.Interfaces;
+
+public interface IGenericRepository<T> where T : BaseEntity
+{
+    public Task<T> GetByIdAsync(int id);
+
+    public Task<IReadOnlyList<T>> GetAllAsync();
+}
