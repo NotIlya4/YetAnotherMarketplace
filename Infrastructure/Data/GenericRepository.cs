73ad34b49456107d92ebbec6b05a0983af3dce09
NotIlya <secretman44444@gmail.com>
2023-01-24 04:42:46 +0300

    Add GenericRepository

---
+++ Infrastructure/Data/GenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+{
+    private readonly StoreContext _context;
+
+    public GenericRepository(StoreContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<T> GetByIdAsync(int id)
+    {
+        return await _context.Set<T>().FirstAsync(p => p.Id == id);
+    }
+
+    public async Task<IReadOnlyList<T>> GetAllAsync()
+    {
+        return await _context.Set<T>().ToListAsync();
+    }
+}
