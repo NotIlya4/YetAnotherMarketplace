82bbd4e2cd7a136ec644aa5627d97903f0457dc4
NotIlya <secretman44444@gmail.com>
2023-01-25 03:34:41 +0300

    Add new specification that add includes for Product entity

---
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities;
+
+namespace Core.Specifications;
+
+public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
+{
+    public ProductsWithTypesAndBrandsSpecification() : base(null)
+    {
+        AddInclude(p => p.ProductType);
+        AddInclude(p => p.ProductBrand);
+    }
+}
