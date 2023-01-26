c992a66ddb27663b5ddf42faf6438d9665c2ca55
NotIlya <secretman44444@gmail.com>
2023-01-26 08:08:37 +0300

    Fix Product entity fields

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -6,8 +6,8 @@ public class Product : BaseEntity
     public required string Description { get; set; }
     public required decimal Price { get; set; }
     public required string PictureUrl { get; set; }
-    public ProductType? ProductType { get; set; }
+    public required ProductType ProductType { get; set; }
     public required int ProductTypeId { get; set; }
-    public ProductBrand? ProductBrand { get; set; }
-    public int ProductBrandId { get; set; }
+    public required ProductBrand ProductBrand { get; set; }
+    public required int ProductBrandId { get; set; }
 }
