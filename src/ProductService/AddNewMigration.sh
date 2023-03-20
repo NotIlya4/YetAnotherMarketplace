df6a63aa01c974eb73a1fe6be85c3d6adfc10091
NotIlya <secretman44444@gmail.com>
2023-03-20 19:33:33 +0300

    Edit add migration script so now it will not close after migration apply

--- src/ProductService/AddNewMigration.sh
+++ src/ProductService/AddNewMigration.sh
@@ -2,3 +2,5 @@ read -p "Enter Migration Name: " migrationName
 
 echo "Adding migration $migrationName..."
 dotnet ef migrations add "$migrationName" --project Infrastructure --startup-project Api -o EntityFramework/Migrations
+
+read -p "Press any key to exit..."
