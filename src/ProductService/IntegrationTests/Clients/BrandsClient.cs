e7447ab61b842a6a90195854492362214f351675
NotIlya <secretman44444@gmail.com>
2023-03-31 12:17:18 +0300

    Add client for brands controller

---
+++ src/ProductService/IntegrationTests/Clients/BrandsClient.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace IntegrationTests.Clients;
+
+public class BrandsClient
+{
+    public HttpClient Client { get; }
+    
+    public BrandsClient(HttpClient client)
+    {
+        Client = client;
+    }
+    
+    public async Task<JObject> PostNewBrand(string brandName)
+    {
+        HttpResponseMessage response = await Client.PostAsync($"api/brands/{brandName}");
+        response.EnsureSuccessStatusCode();
+        
+        return await response.ExtractJObject();
+    }
+
+    public async Task<JArray> GetBrands()
+    {
+        HttpResponseMessage response = await Client.GetAsync("api/brands");
+        response.EnsureSuccessStatusCode();
+        
+        return await response.ExtractJArray();
+    }
+    
+    public async Task DeleteBrand(string brandName)
+    {
+        HttpResponseMessage response = await Client.DeleteAsync($"api/brands/{brandName}");
+        response.EnsureSuccessStatusCode();
+    }
+}
